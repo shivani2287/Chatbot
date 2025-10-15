0b5887e5d78bf8e0078ea9bdde09415ca2089b41
shivani2287 <55627490+shivani2287@users.noreply.github.com>
2025-10-15 23:17:26 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,22 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
-OPENAI_SECRET_KEY = "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"
+key_vault_name = "shivanikeys"
+vault_url = f"https://{key_vault_name}.vault.azure.net/"
+
+# # Authenticate and create a client
+# credential = DefaultAzureCredential()
+# client = SecretClient(vault_url=vault_url, credential=credential)
+
+# # Fetch a secret
+# secret_name = "your-secret-name"
+# retrieved_secret = client.get_secret(secret_name)
+
+#print(f"Secret Value: {retrieved_secret.value}")
+
 #upload pdf file
 st.header("My first chat bot")
 
@@ -23,20 +37,33 @@ if file is not None:
     #st.write(text)  # Display first 500 characters of the extracted text
 
 
-# Break text into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-     separators="\n",
-     chunk_size=1000,
-     chunk_overlap=150,
-     length_function=len
-)
-text_chunks = text_splitter.split_text(text=text)
-#st.write(f"Total Chunks: {len(text_chunks)}")
-#st.write(text_chunks)  # Display the first text chunk
+    # Break text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    text_chunks = text_splitter.split_text(text=text)
+    #st.write(f"Total Chunks: {len(text_chunks)}")
+    #st.write(text_chunks)  # Display the first text chunk
 
-#generate embeddings
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_SECRET_KEY)
+    #generate embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_SECRET_KEY)
 
-#create vector store
-vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-st.write(vectorstore)  # Display vector store information
+    #create vector store
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    st.write(vectorstore)  # Display vector store information
+
+    # get user query
+    user_question = st.text_input("Ask questions about your PDF file:")
+
+    # do similarity search
+    if user_question:
+        docs = vectorstore.similarity_search(user_question)
+        #st.write(docs)  # Display the retrieved documents
+
+        #generate answer
+        # chain = load_qa_chain(OpenAI(temperature=0, openai_api_key=OPENAI_SECRET_KEY), chain_type="stuff")
+        # response = chain.run(input_documents=docs, question=user_question)
+        # st.write(response)  # Display the answer
